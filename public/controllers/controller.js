8e03fec2234e1f2462af69b9ed2cdd1c7638f033
drownedout <danielcdonnelly@gmail.com>
2015-08-16 19:41:59 -0700

    Added database, remove, and add functions

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,4 +1,28 @@
 var myApp = angular.module('myApp',[]);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http){
 	console.log("Hello World");
+
+	var refresh = function() {
+		$http.get('/contactlist').success(function(response){
+			console.log("got da data");
+			$scope.contactList = response;
+			$scope.contact = "";
+		});
+	};
+
+	refresh();
+
+	$scope.addContact = function(){
+		console.log($scope.contact);
+		$http.post('/contactlist', $scope.contact).success(function(response){
+			console.log(response);
+			refresh();
+		});
+	};
+
+	$scope.remove = function(id) {
+		console.log(id);
+
+	}
+
 }]);
--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 var express = require('express');
-
+var mongojs = require('mongojs');
 var app = express();
+var db = mongojs('contactList',['contactList']);
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+app.get('/contactlist', function(req, res) {
+	console.log("Get request received");
+
+	db.contactList.find(function(err, docs){
+		console.log(docs);
+		res.json(docs);
+	});
+});
+
+app.post('/contactlist', function(req, res) {
+	console.log(req.body);
+	db.contactList.insert(req.body, function(err, doc) {
+		res.json(doc);
+	});
+});
 
 app.listen('3000');
 console.log('Server Running');
